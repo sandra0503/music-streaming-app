0a03dfa1463677b238ac3e413884fc2b3220e75d
Sandra Zollner <sandra.b.zollner@gmail.com>
2025-09-28 11:50:18 +0200

    feat: init project
    - inits backend with Express, Mongo DB and docker
    - inits frontend with React

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const User = require('./models/User');
+
+// test route
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Music Streaming App backend!');
+});
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () =>
+      console.log('Backend running on port', process.env.PORT || 5000)
+    );
+  })
+  .catch(err => console.error(err));
